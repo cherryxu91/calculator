77c8ad2a588e9fbfcd2bd70a939daacdb29da6df
Cherry Xu <Cherry@Cherrys-Air.fios-router.home>
2019-07-17 23:51:35 -0400

    loop for 1st and 2nd number, trying to figure out loop for operators

--- index.py
+++ index.py
@@ -1,17 +1,63 @@
-x = float(input('What is the first number?\n'))
-operator = input('What do you want to do with number?\n')
-y = float(input('What is the second number?\n'))
+while True:
+    number1 = input('Please enter a number.\n')
 
-if operator == '+' :
-    print(x + y)
-elif operator == '-' :
-    print(x - y)
-elif operator == '/' :
-    print(x / y)
-elif operator == '*' :
-    print(x * y)
-else:
-    print('Invalid operator. Please try again.')
+    try:
+        number1 = float(number1)
+        break
+
+    except:
+        pass
+
+while True:
+    operator = input('What do you want to do with number?\n')
+    try:
+        if operator == '+' :
+            print(number1 + number2)
+        elif operator == '-' :
+            print(number1 - number2)
+        elif operator == '/' :
+            print(number1 / number2)
+        elif operator == '*' :
+            print(number1 * number2)
+        break
+
+    except:
+        print('Invalid operator. Please try again.')
+        pass
+
+# while True:
+#     operator = input('What do you want to do with number?\n')
+#
+#     try:
+#         operator = '+, -, /, *, or ^'
+#         if operator == '+' :
+#             print(x + y)
+#         elif operator == '-' :
+#             print(x - y)
+#         elif operator == '/' :
+#             print(x / y)
+#         elif operator == '*' :
+#             print(x * y)
+#         elif operator == '^' :
+#             print(x ** y)
+#         break
+#
+#     except:
+#         print('Invalid operator. Please try again.')
+#         pass
+
+# number2 = input('What is the second number?\n')
+#
+while True:
+    number2 = input('Please enter a number.\n')
+
+    try:
+        number2 = float(number2)
+        break
+
+    except:
+        pass
+# above is y = float(input('What is the second number?\n'))
 
 # to do
 # use 'try/except' for each variable to make sure it's a number and not a string
