dca867c742e4831b2daab742f545ced0d0fca547
Cherry Xu <Cherry@Cherrys-Air.fios-router.home>
2019-07-28 23:48:31 -0400

    Possible final product, will look over multiple times

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
-# make an input for 1st number
+# use 'try/except' for each variable to make sure it's a number and not a string
+
+# make a loop so user inputs 1st number
 while True:
     num1 = input('Please enter a number.\n')
 
@@ -28,32 +30,17 @@ while True:
     except:
         pass
 
-# add = (float(number1) + float(number2))
-# subtract = (float(number1) - float(number2))
-# divide = (float(number1) / float(number2))
-# multiply = (float(number1) * float(number2))
-
 # make a function to call operator after entering num1, num2, and operator
 def operation(num1, operator, num2):
-    operator == '+', '-', '/', '*'
-# set operators as variables in function
-    add = num1 + num2
-    subtract = num1 - num2
-    divide = num1 / num2
-    multiply = num1 * num2
-    if  operator == '+' :
-        print(num1, '+', num2, '= ', add)
+    if operator == '+' :
+        return num1 + num2
     elif operator == '-' :
-        print(num1, '-', num2, '= ', subtract)
+        return num1 - num2
     elif operator == '/' :
-        print(num1, '/', num2, '= ', divide)
+        return num1 / num2
     elif operator == '*' :
-        print(num1, '*', num2, '= ', multiply)
+        return num1 * num2
 
-operation(num1, operator, num2)
-print(num1)
-print(operator)
-print(num2)
-# to do
-# use 'try/except' for each variable to make sure it's a number and not a string
-# make a loop for operator and use 'try/except' too
+result = operation(num1, operator, num2)
+finalans = round(result, 2)
+print(finalans)
